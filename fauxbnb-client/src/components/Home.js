803a41f567b57e748d6b1d13794083247b9ea107
aysan <aisayo@icloud.com>
2020-09-30 17:00:14 -0700

    added dropdown to navbar

--- fauxbnb-client/src/components/Home.js
+++ fauxbnb-client/src/components/Home.js
@@ -7,7 +7,6 @@ const Home = () => {
     return (
         <>
             <Image fluid alt="HomePage Img" src="https://images.unsplash.com/photo-1566779342022-8e33031b622c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1867&q=80" />
-            <UserLogin />
         </>
     );
 };
--- fauxbnb-client/src/components/routing/Navbar.js
+++ fauxbnb-client/src/components/routing/Navbar.js
@@ -1,24 +1,46 @@
 import React from "react";
 
-import { Navbar as NavBar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import {
+  Navbar as NavBar,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+  NavDropdown,
+} from "react-bootstrap";
 
 const Navbar = () => {
   return (
     <>
-      <NavBar bg="light" variant="light" >
+      <NavBar bg="light" variant="light">
         <NavBar.Brand href="/">FauxBnB</NavBar.Brand>
-        <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link href="/posts">Posts</Nav.Link>
-        <Nav.Link href="/users/login">Login</Nav.Link>
-        <Nav.Link href="/users/signup">Signup</Nav.Link>
+        <Nav className="mr-auto">
+          <Nav.Link href="/posts">Places</Nav.Link>
+        </Nav>
+
+        <NavDropdown title="Want to Host?" id="basic-nav-dropdown">
+          <NavDropdown.Item href="/users/signup">Signup</NavDropdown.Item>
+          <NavDropdown.Item href="/users/login">Login</NavDropdown.Item>
+          <NavDropdown.Item href="/posts/new">Add a new place</NavDropdown.Item>
+        </NavDropdown>
 
         <Form inline>
-          <FormControl type="text" className="mr-sm-2" />
+          <FormControl
+            type="text"
+            className="mr-sm-2"
+            placeHolder="Start your search"
+          />
           <Button variant="outline-dark">Search</Button>
         </Form>
       </NavBar>
     </>
   );
+};
+
+const searchStyle = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "flex-end",
 };
 
 export default Navbar;
