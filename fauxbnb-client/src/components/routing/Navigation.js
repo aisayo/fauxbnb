cd723c9dbf6a97e00775ecb80dd0b6500b489ae9
aysan <aisayo@icloud.com>
2020-10-01 08:43:21 -0700

    fixed collapse bug with navbar

--- fauxbnb-client/src/components/routing/Navigation.js
+++ fauxbnb-client/src/components/routing/Navigation.js
@@ -1,32 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 import Searchbar from "../Searchbar";
 
 const Navigation = () => {
   return (
-    <Navbar bg="light" variant="light" fill expand="lg">
-      <Navbar.Toggle aria-controls="basic-Navbar-nav" />
-
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Navbar.Brand href="/">FauxBnB</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Nav className="ml-auto">
-          <Nav.Item>
-            <Nav.Link>
-              <Link to="/posts">Posts</Link>
-            </Nav.Link>
-          </Nav.Item>
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+      <Navbar.Brand href="/">FauxBnB</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/posts">Places</Nav.Link>
+          <NavDropdown title="Want to host?" id="collapsible-nav-dropdown">
+            <NavDropdown.Item href="/users/signup">Signup</NavDropdown.Item>
+            <NavDropdown.Item href="/users/login"> Login </NavDropdown.Item>
+            <NavDropdown.Item href="/postings/new">Add a new place</NavDropdown.Item>
+          </NavDropdown>
         </Nav>
-        <NavDropdown title="Want to Host?" id="basic-nav-dropdown">
-          <NavDropdown.Item href="/users/signup">Signup</NavDropdown.Item>
-          <NavDropdown.Item href="/users/login">Login</NavDropdown.Item>
-          <NavDropdown.Item href="/posts/new">Add a new place</NavDropdown.Item>
-        </NavDropdown>
-        <Searchbar />
       </Navbar.Collapse>
+      <Searchbar />
     </Navbar>
   );
 };
