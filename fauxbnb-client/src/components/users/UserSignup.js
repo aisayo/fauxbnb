400e1d631439fcee5f5a764c1b0578470d6dd0d8
aysan <aisayo@icloud.com>
2020-10-01 17:02:11 -0700

    refactored signup form

--- fauxbnb-client/src/components/users/UserSignup.js
+++ fauxbnb-client/src/components/users/UserSignup.js
@@ -1,41 +1,69 @@
 import React, { useState } from 'react';
+import { Form, Button, FormGroup, FormControl } from "react-bootstrap";
 
 import TextField from '../common/TextField'
 
 const UserSignup = () => {
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [userData, setUserData] = useState({ email: '', password: ''})
 
+    const handleUserData = e => {
+      setUserData({
+          ...userData,
+          [e.target.name]: e.target.value
+      })
+    }
+  
     return (
-       <form>
-
-           <label>Email: </label>
-           <TextField 
-                type='text'    
-                value={email}
-                onChange={e => setEmail(e.target.value)}            
+      <div style={divStyle}>
+        <h1>Sign Up</h1>
+        <Form>
+          <FormGroup controlId="formBasicEmail">
+            <Form.Label>Email: </Form.Label>
+            <FormControl
+              type="email"
+              value={userData.email}
+              onChange={handleUserData}
+              name="email"
             />
-
-            <br/>
-            <br/>
-            
-            <label>Password: </label>
-            <TextField
-                type='password'
-                value={password}
-                onChange={e => setPassword(e.target.value)}            
+          </FormGroup>
+  
+          <br />
+          <br />
+  
+          <FormGroup controlId="formBasicPassword">
+            <Form.Label>Password: </Form.Label>
+  
+            <FormControl
+              type="password"
+              value={userData.password}
+              onChange={handleUserData}
+              name="password"
             />
-            <br/>
-            <br/>
-
-            <input 
-                type='submit'
-                value='Login'                
-            />
-
-       </form>
+          </FormGroup>
+          <br />
+          <br />
+  
+          <Button variant="light" type="submit">
+            Sign Up
+          </Button>
+        </Form>
+      </div>
     );
-};
-
+  };
+  
+  const divStyle = {
+    border: "solid",
+    borderColor: "lightGray",
+    display: "flex",
+    flexDirection: "column",
+    alignContent: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "40%",
+    marginTop: "5%",
+    marginLeft: "25%",
+    paddingTop: "5%",
+    paddingBottom: "5%",
+  };
 export default UserSignup;
